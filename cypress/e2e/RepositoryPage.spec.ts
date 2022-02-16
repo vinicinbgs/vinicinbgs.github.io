1d4bd6d9f681b12da39265262c6aabff3f9ceb2d
Vinicius Dutra <dutra_morais@hotmail.com>
2022-02-16 01:17:36 -0300

    refactor: add enviroments variables in cypress

--- cypress/e2e/RepositoryPage.spec.ts
+++ cypress/e2e/RepositoryPage.spec.ts
@@ -8,9 +8,10 @@ describe('Accessibility tests', () => {
   it('Visit Repository Page', () => {
     cy.get('#repositories').should('be.visible');
 
-    cy.intercept('GET', 'https://api.github.com/users/vinicinbgs/repos').as(
-      'repos'
-    );
+    cy.intercept(
+      'GET',
+      `${Cypress.env('GATSBY_API_GITHUB')}/users/vinicinbgs/repos`
+    ).as('repos');
 
     cy.waitFor('@repos');
     cy.get('[data-type="repository"]').should('be.visible');
