543fe19922fa0ee82cc055efcdc87d5a85ddfe77
Vinicius Morais Dutra <vmdutra.ciet@rd.com.br>
2022-02-26 02:15:50 -0300

    fix: deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,9 @@ cd public && \
 git init && \
 git remote add origin https://github.com/vinicinbgs/vinicinbgs.github.io.git && \
 git checkout -b gh-pages && \
+git fetch && \
 git add . &&\
 git commit -m "deploy" && \
-git push -u origin gh-pages -f && \
+git push -u origin gh-pages -f --allow-unrelated-histories && \
 cd .. && \
 git checkout develop
