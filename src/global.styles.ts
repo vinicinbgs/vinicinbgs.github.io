eb2f1a0fa55a9c623406d80e2d96b2e896f7f765
Vinicius Dutra <dutra_morais@hotmail.com>
2022-07-18 20:54:17 -0300

    refactor: add shimmer repositories

--- src/global.styles.ts
+++ src/global.styles.ts
@@ -26,4 +26,30 @@ const PageDivided = styled.div`
   justify-content: space-between;
 `;
 
-export { Section, Title, PageDivided };
+const SkeletonText = styled.div`
+  width: 100%;
+  min-width: 100px;
+  height: 0.7rem;
+  margin-top: 10px;
+  margin-bottom: 0.5rem;
+  border-radius: 0.25rem;
+  animation: placeholderShimmer 4s linear infinite forwards;
+  background: linear-gradient(to right,
+    var(--theme-ui-colors-gray-1) 0%,
+    var(--theme-ui-colors-gray-2) 50%,
+    var(--theme-ui-colors-gray-3) 70%,
+    var(--theme-ui-colors-gray-4) 100% 
+  );
+  
+  @keyframes placeholderShimmer {
+  0% {
+    background-position: -468px 0;
+  }
+  
+  100% {
+    background-position: 468px 0; 
+  }
+}
+`;
+
+export { Section, Title, PageDivided, SkeletonText };
